a782076b68e18c7587a7a53fc8c22fd721b7fa99
killer <163946666+younglum@users.noreply.github.com>
2024-09-04 15:19:57 +0900

    Add files via upload

---
+++ Python/0904/# week02_03.py
@@ -0,0 +1,32 @@
+# week02_03.py
+
+# sep옵션은 교재에 없음
+print("I", "AM", "A", "BOY")
+print("I", "AM", "A", "BOY",sep="")
+print("I", "AM", "A", "BOY",sep="/")
+print("I", "AM", "A", "BOY",sep="\n")
+
+print("="*10)
+
+print()
+print(int(1))
+print(str("1"))
+print(float(1.1))
+print(bool(True))
+print([1,2,3])
+print("123")
+print("1" "2" "3")
+print("1","2","3")
+print("1"+"2"+"3")
+
+print("I")
+print("AM")
+print("A")
+print("BOY.")
+
+print("I",end=" ")
+print("AM", end="\n")
+print("A",end="~~")
+print("BOY.")
+
+print("="*10)
