54a3a61360b217ce8f0d05e3fd807438f6293498
killer <163946666+younglum@users.noreply.github.com>
2024-09-04 14:45:49 +0900

    Add files via upload

---
+++ Python/0904/week02_02.py
@@ -0,0 +1,29 @@
+# week02_02.py
+
+print("#4")
+score1=input("점수1 :")
+score2=input("점수2 :")
+print(score1+score2)
+
+print("#5")
+score1=int(input("점수 1:"))
+score2=int(input("점수2 :"))
+print(score1+score2)
+
+
+print("#1")
+input()
+
+print("#2")
+input("점수:")
+print("#3")
+a=input("점수:")
+print(a)
+
+now_old=int(input("올해 나이를 입력하시오:"))
+new_year_old=int(input("내년 나이를 입력하시오:"))
+print("올해 나이는 %d살 입니다",now_old)
+print("내년 나이는 %d살 입니다",new_year_old)
+
+age=input("나이:")
+print("내년 나이:%d",int(age)+1)
