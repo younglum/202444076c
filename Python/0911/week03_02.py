1f023d917906cdb779e45766684345eea11ef37b
killer <163946666+younglum@users.noreply.github.com>
2024-09-11 14:42:57 +0900

    Add files via upload

---
+++ Python/0911/week03_02.py
@@ -0,0 +1,22 @@
+"""해당 코드는 3주차 2번째 코드입니다.
+저는 임수영이고 잘 먹고 있습니다.
+저는 여러 줄 주석을 흉내냅니다."""
+
+a=str("Hello World")
+c='Hello World'
+d="""Hello
+World"""
+f= "Hello World'"
+print(a)
+print(c)
+print(d)
+print(f)
+
+print("*"*10)
+str="안녕\n나는 \"김인하\"라고 해.\t c://test//test.py"
+print(str)
+
+#위에서 str을 class로 만들어 버렸기 때문에 아래에서 str의 기능을 사용못함
+
+z = str('a')   
+print(z)
