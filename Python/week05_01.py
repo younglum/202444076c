9bffd6654e7124745d98a274cf5cf82d956bbd5f
killer <163946666+younglum@users.noreply.github.com>
2024-09-25 13:43:36 +0900

    Add files via upload

---
+++ Python/week05_01.py
@@ -0,0 +1,15 @@
+class test:
+    def __init__(self):
+        self.a=1
+        self.b=2
+
+test1= [1,2]
+test2 =(1,2)
+test3=test()
+
+print(test1[0])
+print(test2[0])
+print(test3.a)
+print(test1[1])
+print(test2[1])
+print(test3.b)
